03a555c0796c20d344beee4861eb8b12797a945d
Anonyhito <108658785+Anonyhito@users.noreply.github.com>
2025-07-15 15:41:35 +0800

    task04

--- task04.js
+++ task04.js
@@ -1,25 +1,13 @@
 "use strict";
 
-// TODO: Write if/else if to check and print:
-// Divisible by both 3 and 5 → "FizzBuzz"
-// Divisible by only 3 → "Fizz"
-// Divisible by only 5 → "Buzz"
-// Otherwise → "Not divisible by 3 or 5"
-
-// TODO BONUS: Rewrite with if
-
 const num = 15;
 
 if (num % 3 === 0 && num % 5 === 0) {
-  console.log("FizzBuzz");
+    console.log("FizzBuzz");
 } else if (num % 3 === 0) {
-  console.log("Fizz");
+    console.log("Fizz");
 } else if (num % 5 === 0) {
-  console.log("Buzz");
+    console.log("Buzz");
 } else {
-  console.log("Not divisible by 3 or 5");
-
-
-//bonus
-
-
+    console.log("Not divisible by 3 or 5");
+}
